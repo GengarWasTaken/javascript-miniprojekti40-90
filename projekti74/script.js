5f15ee8b10b40ef725fa6cc3d3278d95917cc429
GengarWasTaken <72507176+GengarWasTaken@users.noreply.github.com>
2021-02-14 10:56:14 +0100

    Add files via upload

---
+++ projekti74/script.js
@@ -0,0 +1,58 @@
+const imgs = document.getElementById("imgs");
+
+const leftButton = document.getElementById("left");
+
+const rightButton = document.getElementById("right");
+
+const img = document.querySelectorAll("#imgs img");
+
+let index = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+
+    index++;
+    changeImage();
+
+}
+
+function changeImage() {
+
+    if (index > img.length - 1) {
+
+        index = 0;
+
+    } else if (index < 0) {
+
+        index = img.length - 1;
+
+    }
+
+    imgs.style.transform = `translateX(${-index * 500}px)`;
+
+}
+
+function resetInterval() {
+
+    clearInterval(interval);
+
+    interval = setInterval(run, 2000)
+
+}
+
+rightButton.addEventListener("click", () => {
+
+    index++;
+    changeImage();
+    resetInterval();
+
+})
+
+leftButton.addEventListener("click", () => {
+
+    index--;
+    changeImage();
+    resetInterval();
+
+})
